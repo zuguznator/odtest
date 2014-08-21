bd3a9f36a58604a0a26926d8159fe2b194a5fd4a
Hugo Romo <zuguznator@gmail.com>
2014-08-20 18:59:20 -0700

    Signed-off-by: Hugo Romo <zuguznator@gmail.com>
    *Template app for od like command (HugoRomo)

--- main.c
+++ main.c
@@ -22,13 +22,20 @@
 #include <fcntl.h>
 #include <ctype.h>
 
+/* Modified by Hugo Ivan Romo Villaseñor */
+
+#define DATA_LENGTH 16u
+#define NULL_VALUE 0u
+
 int
 main(int argc, char **argv)
 {
-    int fd;
+    int fd, index, dir;
     char *file = NULL;
-    unsigned char buff[16];
-
+    unsigned char buff[DATA_LENGTH];
+    struct stat file_info;
+    size_t* length;
+    dir = NULL_VALUE;
     if(argc != 2)
     {
         printf ("Usage: %s <file>\n", argv[0]);
@@ -44,11 +51,56 @@ main(int argc, char **argv)
         printf ("Error");
         exit(1);
     }
+       /* Print complete file content following od hex format */
+       /* od -A x -t x1z -v file.dat */
+       /* Get information about the file.  */
+       fstat (fd, &file_info);
+       /* Get length of the file.  */
+       *length = file_info.st_size;
+	/*dir will check for all data*/
+	while (dir<*length)
+       {
+	/* Read 16 characters per lap */
+	read(fd,buff,DATA_LENGTH);
+	/* Print direction with 6 digits */
+	printf("%06x ",dir);
+	/* Check if is not the last 16 bytes */
+	if((*length - dir) >= DATA_LENGTH)
+	{
+		for(index=NULL_VALUE;index<DATA_LENGTH;index++)
+		{
+			/* Print 16 hex values*/
+			printf("%02x ",buff[index]);
+		}
+	}
+	else
+	{
+		for(index=NULL_VALUE;index<(*length - dir);index++)
+		{
+			/* Print valid values only */
+			printf("%02x ",buff[index]);
+		}
+		for(index=(*length - dir);index<DATA_LENGTH;index++)
+		{
+			/* Print spaces instead of invalid values */
+			printf("   ");
+			/* Set spaces instead of invalid values */
+			buff[index] = 0; /* " " */
+		}
+	}
+		for(index=NULL_VALUE;index<DATA_LENGTH;index++)
+		{
+			/* Replace "Backspace"(0x0A) with "dots"(46) */
+			if(0x0A  == buff[index]) 
+			{
+				buff[index] = 46; /* "." */
+			}
+		}
+		/* Print characters*/
+		printf(">%s<\n",buff);
+		dir += DATA_LENGTH;
+        }
+	close (fd);
 
-    /* Print complete file content following od hex format */
-    /* od -A x -t x1z -v file.dat */
-
-    close (fd);
-
-    return 0;
+    return NULL_VALUE;
 }
